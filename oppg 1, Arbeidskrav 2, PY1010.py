770f0957767f4dd6cd535d4e98757df3c8d67a62
JakobSodal <jakobsodal@hotmail.com>
2025-01-15 21:29:33 +0100

    Add files via upload

---
+++ oppg 1, Arbeidskrav 2, PY1010.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 14:50:54 2024
+
+@author: Jakob Sødal
+"""
+'''
+Arbeidskrav 2  PY1010
+
+Oppg 1)
+
+Program som regner ut alder i løpet av 2024.
+'''
+
+#%%
+fødselsår = int(input('Hvilket år er du født?')) # bruker skriver inn fødselsår
+
+alder = int(2024-fødselsår) # utregning av alder i løpet av år 2024.
+
+print('I løpet av 2024 er eller vil din alder bli:',alder ,'år')
